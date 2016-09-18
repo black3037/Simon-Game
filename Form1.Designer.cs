aadd1847d389c8ad05e25248a98c990b05e46d56
Derek Black <black@ksu.edu>
2016-09-18 14:59:37 -0500

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace Simon
+{
+    partial class Simon
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Simon));
+            this.Red = new System.Windows.Forms.Button();
+            this.Blue = new System.Windows.Forms.Button();
+            this.Green = new System.Windows.Forms.Button();
+            this.Yellow = new System.Windows.Forms.Button();
+            this.Start = new System.Windows.Forms.Button();
+            this.roundNumberText = new System.Windows.Forms.Label();
+            this.RoundNumberSet = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Red
+            // 
+            this.Red.BackColor = System.Drawing.Color.Salmon;
+            this.Red.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.Red.Location = new System.Drawing.Point(20, 18);
+            this.Red.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Red.Name = "Red";
+            this.Red.Size = new System.Drawing.Size(303, 251);
+            this.Red.TabIndex = 0;
+            this.Red.UseVisualStyleBackColor = false;
+            this.Red.Click += new System.EventHandler(this.Red_Click);
+            // 
+            // Blue
+            // 
+            this.Blue.BackColor = System.Drawing.Color.SkyBlue;
+            this.Blue.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.Blue.Location = new System.Drawing.Point(333, 18);
+            this.Blue.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Blue.Name = "Blue";
+            this.Blue.Size = new System.Drawing.Size(303, 251);
+            this.Blue.TabIndex = 1;
+            this.Blue.UseVisualStyleBackColor = false;
+            this.Blue.Click += new System.EventHandler(this.Blue_Click);
+            // 
+            // Green
+            // 
+            this.Green.BackColor = System.Drawing.Color.SpringGreen;
+            this.Green.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.Green.Location = new System.Drawing.Point(20, 278);
+            this.Green.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Green.Name = "Green";
+            this.Green.Size = new System.Drawing.Size(303, 251);
+            this.Green.TabIndex = 2;
+            this.Green.UseVisualStyleBackColor = false;
+            this.Green.Click += new System.EventHandler(this.Green_Click);
+            // 
+            // Yellow
+            // 
+            this.Yellow.BackColor = System.Drawing.Color.Moccasin;
+            this.Yellow.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.Yellow.Location = new System.Drawing.Point(333, 278);
+            this.Yellow.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Yellow.Name = "Yellow";
+            this.Yellow.Size = new System.Drawing.Size(303, 251);
+            this.Yellow.TabIndex = 3;
+            this.Yellow.UseVisualStyleBackColor = false;
+            this.Yellow.Click += new System.EventHandler(this.Yellow_Click);
+            // 
+            // Start
+            // 
+            this.Start.Location = new System.Drawing.Point(513, 583);
+            this.Start.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Start.Name = "Start";
+            this.Start.Size = new System.Drawing.Size(124, 35);
+            this.Start.TabIndex = 4;
+            this.Start.Text = "Start";
+            this.Start.UseVisualStyleBackColor = true;
+            this.Start.Click += new System.EventHandler(this.Start_Click);
+            // 
+            // roundNumberText
+            // 
+            this.roundNumberText.AutoSize = true;
+            this.roundNumberText.Location = new System.Drawing.Point(12, 583);
+            this.roundNumberText.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.roundNumberText.Name = "roundNumberText";
+            this.roundNumberText.Size = new System.Drawing.Size(67, 20);
+            this.roundNumberText.TabIndex = 5;
+            this.roundNumberText.Text = "Round:";
+            // 
+            // RoundNumberSet
+            // 
+            this.RoundNumberSet.AutoSize = true;
+            this.RoundNumberSet.Location = new System.Drawing.Point(12, 608);
+            this.RoundNumberSet.Name = "RoundNumberSet";
+            this.RoundNumberSet.Size = new System.Drawing.Size(19, 20);
+            this.RoundNumberSet.TabIndex = 6;
+            this.RoundNumberSet.Text = "0";
+            // 
+            // Simon
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(659, 636);
+            this.Controls.Add(this.RoundNumberSet);
+            this.Controls.Add(this.roundNumberText);
+            this.Controls.Add(this.Start);
+            this.Controls.Add(this.Yellow);
+            this.Controls.Add(this.Green);
+            this.Controls.Add(this.Blue);
+            this.Controls.Add(this.Red);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(675, 675);
+            this.MinimumSize = new System.Drawing.Size(675, 675);
+            this.Name = "Simon";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Simon";
+            this.Load += new System.EventHandler(this.Simon_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Red;
+        private System.Windows.Forms.Button Blue;
+        private System.Windows.Forms.Button Green;
+        private System.Windows.Forms.Button Yellow;
+        private System.Windows.Forms.Button Start;
+        private System.Windows.Forms.Label roundNumberText;
+        private System.Windows.Forms.Label RoundNumberSet;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace Simon
+{
+    public partial class Simon : Form
+    {
+
+        // Initialize Starting Round
+        int roundNumber = 0;
+        int level = 0;
+
+        // Initial states of player and ai
+        static List<int> aiState = new List<int>();
+        List<int> playerState = new List<int>();
+        bool aiPlaying = false;
+        Random rnd = new Random();
+
+        
+
+        public Simon()
+        {
+            InitializeComponent();
+        }
+
+        private void Simon_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        void play()
+        {
+            int delayTimer = 250;
+
+            // Start ai round
+            aiPlaying= true;
+
+            // Iterate through aiStates to light up the appropriate pattern to user
+            foreach(int color in aiState)
+            {
+                switch(color)
+                {
+                    case 0:
+                        new Thread(beep1).Start();
+
+                        Red.BackColor = Color.White;
+                        Thread.Sleep(delayTimer);
+                        Red.BackColor = Color.Salmon;
+                        break;
+                    case 1:
+                        new Thread(beep2).Start();
+                        Blue.BackColor = Color.White;
+                        Thread.Sleep(delayTimer);
+                        Blue.BackColor = Color.SkyBlue;
+                        break;
+                    case 2:
+                        new Thread(beep3).Start();
+                        Green.BackColor = Color.White;
+                        Thread.Sleep(delayTimer);
+                        Green.BackColor = Color.SpringGreen;
+                        break;
+                    case 3:
+                        new Thread(beep4).Start();
+                        Yellow.BackColor = Color.White;
+                        Thread.Sleep(delayTimer);
+                        Yellow.BackColor = Color.Moccasin;
+                        break;
+                }
+                Thread.Sleep(delayTimer);
+            }
+
+            // End ai round
+            aiPlaying = false;
+
+        }
+
+        /// <summary>
+        /// These functions send sound to the user once a button is pressed or the ai is playing
+        /// </summary>
+        void beep1()
+        {
+            Console.Beep(250, 250);
+        }
+
+        void beep2()
+        {
+            Console.Beep(375, 250);
+        }
+
+        void beep3()
+        {
+            Console.Beep(425, 250);
+        }
+
+        void beep4()
+        {
+            Console.Beep(620, 250);
+        }
+
+
+        // Set up colored button functions
+        private void Red_Click(object sender, EventArgs e)
+        {
+            new Thread(beep1).Start();
+            playerPlaying(0);
+
+        }
+
+        private void Blue_Click(object sender, EventArgs e)
+        {
+            new Thread(beep2).Start();
+            playerPlaying(1);
+        }
+
+        private void Green_Click(object sender, EventArgs e)
+        {
+            new Thread(beep3).Start();
+            playerPlaying(2);
+        }
+
+        private void Yellow_Click(object sender, EventArgs e)
+        {
+            new Thread(beep4).Start();
+            playerPlaying(3);
+        }
+        
+
+        // Player's turn function
+        void playerPlaying(int color)
+        {
+            // Don't append data if ai is currently playing and user pushes a button
+            if (aiPlaying == true)
+            {
+                return;
+            }
+            // Collect color pattern from the user
+            playerState.Add(color);
+
+            // Check to make sure players pattern and ai's patter are same length
+            if (playerState.Count() == aiState.Count())
+            {
+                // Check players pattern against ai's pattern and advance/start over round.
+                if (playerState.SequenceEqual(aiState) == true)
+                {
+                    DialogResult result = MessageBox.Show("Round Passed! Play Next Round?", "Simon", MessageBoxButtons.YesNo);
+                    if (result == DialogResult.Yes)
+                    {
+                        Thread.Sleep(200);
+                        Start.PerformClick();
+                    }
+                    else if (result == DialogResult.No)
+                    {
+                        System.Windows.Forms.Application.Exit();
+                    }
+                    
+                }
+                else
+                {
+                    DialogResult result = MessageBox.Show("Round Failed! Play Again?", "Simon", MessageBoxButtons.YesNo);
+                    if (result == DialogResult.Yes)
+                    {
+                        roundNumber = 0;
+                        Thread.Sleep(200);
+                        Start.PerformClick();
+                    }
+                    else if (result == DialogResult.No)
+                    {
+                        System.Windows.Forms.Application.Exit();
+                    }
+                }
+
+            }
+        }
+
+        // Game start button
+        private void Start_Click(object sender, EventArgs e)
+        {
+            // Clear all lists for the start of each round
+            aiState.Clear();
+            playerState.Clear();
+
+            // Generate new ai states for the round
+            for (int i = 0; i < roundNumber + 1; i++)
+            {
+                aiState.Add(rnd.Next(0, 4));
+            }
+            
+            // Start round and incremement round number
+            new Thread(play).Start();
+            roundNumber++;
+            RoundNumberSet.Text = roundNumber.ToString();
+        }
+    }
+}
